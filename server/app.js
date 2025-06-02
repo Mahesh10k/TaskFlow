c18b9bf39d46165af5af8a1b1ba44c494a5fba3b
Mahesh10K <maheshpikki3049@gmail.com>
2025-06-02 16:43:51 +0530

    added corsOptions

--- server/app.js
+++ server/app.js
@@ -10,17 +10,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// var whitelist = [ 'https://localhost:5173', 'https://task-flow-hk9bqqepq-mahesh10ks-projects.vercel.app',"https://task-flow.vercel.app"]
-// var corsOptions = {
-//   origin: function (origin, callback) {
-//     if (whitelist.indexOf(origin) !== -1) {
-//       callback(null, true)
-//     } else {
-//       callback(new Error('Not allowed by CORS'))
-//     }
-//   }
-// }
-// app.use(cors(corsOptions))
+var whitelist = [ 'https://localhost:5173', 'https://task-flow-9uwx.vercel.app',"https://task-flow.vercel.app"]
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+app.use(cors(corsOptions))
 
 app.use(express.urlencoded({extended:true}))
 
